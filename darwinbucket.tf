ad10f4a7f97bc747bd475a169d86d1db72371ccf
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-22 21:28:01 +0530

    changes in setup of terraform over workflow

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -2,38 +2,18 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_s3_bucket" "darwin_bucket" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
 
-
-resource "aws_s3_bucket" "s3-bucket" {
-    provider = aws.us-east-1
-    bucket = "digitalrw-"
-    acl    = "private"
-    versioning {enabled = true}
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm     = "AES256"
-            }
-        }
-    }
-    lifecycle_rule {
-        id = "CleanOldVersion"
-        prefix  = ""
-        enabled = true
-        abort_incomplete_multipart_upload_days = 1
-        noncurrent_version_expiration {
-            days = 14
-        }
-    }
-    tags = {
-        ApplicationID             = " PHYLOv2"
-        Environment               = var.env
-        PrimaryApplicationOwner   = "0"
-        CostCenter                = "0"
-        ProjectID                 = "0"
-        Expiration                = "0"
-        MIO                       = "0"
-        ManagedBy                 = "Terraform"
-        APP_GEN1                  = "S3-PHYLOv2" 
-    }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
+
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+}
+
