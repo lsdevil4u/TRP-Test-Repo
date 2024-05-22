80af61dae55b5f0b64914405a6209d02dd29b56d
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-22 22:30:07 +0530

    changes in terraform file

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -1,19 +1,42 @@
 provider "aws" {
-  region = var.region
+
+region = "us-east-1"
+profile = "default"
 }
 
-resource "aws_s3_bucket" "darwin_bucket" {
-  bucket = "my-unique-bucket-name"
-  acl    = "private"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 
-variable "region" {
-  description = "The AWS region to deploy resources"
-  type        = string
+resource "aws_s3_bucket" "s3-bucket" {
+    provider = aws.us-east-1
+    bucket = "digitalrw-"
+    acl    = "private"
+    versioning {enabled = true}
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm     = "AES256"
+            }
+        }
+    }
+    lifecycle_rule {
+        id = "CleanOldVersion"
+        prefix  = ""
+        enabled = true
+        abort_incomplete_multipart_upload_days = 1
+        noncurrent_version_expiration {
+            days = 14
+        }
+    }
+    tags = {
+        ApplicationID             = " PHYLOv2"
+        Environment               = var.env
+        PrimaryApplicationOwner   = "0"
+        CostCenter                = "0"
+        ProjectID                 = "0"
+        Expiration                = "0"
+        MIO                       = "0"
+        ManagedBy                 = "Terraform"
+        APP_GEN1                  = "S3-PHYLOv2" 
+    }
 }
 
