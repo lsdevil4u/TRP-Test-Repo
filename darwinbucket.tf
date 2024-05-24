200b93bff0d497c95b632f96cfaa7a51e362f0bf
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-24 12:22:53 +0530

    sample tf code running again

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -1,14 +1,35 @@
 provider "aws" {
-  region = "us-east-1"
-  profile = "default"
+
+region = "us-east-1"
+profile = "default"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-trp-test-bucket"  # Replace with a unique bucket name
-  acl    = "private"
+resource "aws_instance" "os1"{
+
+ami = "ami-0bb84b8ffd87024d8"
+instance_type = "t2.micro"
+tags = {
+ Name = "FaceR-check-instance"
+  }
+}
+
+
+output "my_os1_public_ip_is" {
+value = aws_instance.os1.public_ip
+}
+
+resource "aws_ebs_volume" "st1" {
+  availability_zone = aws_instance.os1.availability_zone
+  size              = 5
 
   tags = {
-    Name        = "MyBucket"
-    Environment = "Dev"
+    Name = "CV-check-Volume"
   }
+}
+
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.st1.id
+  instance_id = aws_instance.os1.id
 }
