b404b953318cd410e8c92a2560210aeb3028369d
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-24 12:17:46 +0530

    sample tf code running

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -1,45 +1,14 @@
 provider "aws" {
-  alias  = "us-east-1"
   region = "us-east-1"
   profile = "default"
 }
 
-resource "aws_s3_bucket" "s3-bucket" {
-  provider = aws.us-east-1
-  bucket   = "digitalrw-"
-  acl      = "private"
-  
-  versioning {
-    enabled = true
-  }
-  
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  
-  lifecycle_rule {
-    id = "CleanOldVersion"
-    prefix  = ""
-    enabled = true
-    abort_incomplete_multipart_upload_days = 1
-    noncurrent_version_expiration {
-      days = 14
-    }
-  }
-  
+resource "aws_s3_bucket" "example" {
+  bucket = "my-trp-test-bucket"  # Replace with a unique bucket name
+  acl    = "private"
+
   tags = {
-    ApplicationID             = "PHYLOv2"
-    Environment               = var.env
-    PrimaryApplicationOwner   = "0"
-    CostCenter                = "0"
-    ProjectID                 = "0"
-    Expiration                = "0"
-    MIO                       = "0"
-    ManagedBy                 = "Terraform"
-    APP_GEN1                  = "S3-PHYLOv2"
+    Name        = "MyBucket"
+    Environment = "Dev"
   }
 }
