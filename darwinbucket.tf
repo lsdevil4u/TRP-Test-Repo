76f3937442ec1b05553eeedac5bad80e56278e8f
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-27 02:52:49 +0530

    changes in new code

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -16,12 +16,3 @@ module "s3_bucket" {
   }
 }
 
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = module.s3_bucket.bucket_id
-}
-
-output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = module.s3_bucket.bucket_arn
-}
