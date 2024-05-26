0e4f3731c7641aa8e3ce80b32be44110b94112e3
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-27 02:48:37 +0530

    changes in new code

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -1,18 +1,28 @@
 provider "aws" {
+  region = "us-east-1"  # Specify the AWS region
 }
 
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.6.0"  # Specify the version of the module
 
-  bucket = "my-s3-bucket12"
+  bucket = "my-s3-bucket1234"
   acl    = "private"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
-  
-  force_destroy = false
-  versioning    = true
-  
-  
+
+  versioning = {
+    enabled = true
+  }
+}
+
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = module.s3_bucket.bucket_id
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = module.s3_bucket.bucket_arn
 }
