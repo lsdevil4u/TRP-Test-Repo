525078e4cc6ff26008028f8e88a2e3586db4a7a0
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-26 20:21:37 +0530

    added in new source

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -2,9 +2,10 @@ provider "aws" {
 }
 
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source = "terraform-aws-modules/s3-bucket/aws",
+  source = "git@github.com:lsdevil4u/TRP-test-repo-module.git"
 
-  bucket = "my-s3-bucket1111"
+  bucket = "my-s3-bucket1112"
   acl    = "private"
 
   control_object_ownership = true
