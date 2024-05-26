4e470cf8b041363da46ae4abaea8a9080b943900
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-27 03:18:30 +0530

    versioning enable and server access logging

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-  }
+  region = "us-east-1"  # Specify the AWS region if needed
+}
 
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.6.0"  # Specify the version of the module
 
   bucket = "digitalrnd-gxp-genstab-prod-virginia-trp-27may2024"
   acl    = "private"
@@ -13,5 +15,20 @@ module "s3_bucket" {
   versioning = {
     enabled = true
   }
+
+  logging = {
+    target_bucket = "my-s3-bucket1111"  # Replace with your logging target bucket
+    # target_prefix = ""  # Omitting this line will save logs directly in the target bucket
+  }
 }
 
+# Ensure the target logging bucket exists
+resource "aws_s3_bucket" "logs_bucket" {
+  bucket = "my-s3-bucket1111"
+  acl    = "log-delivery-write"
+
+  # Optionally, enable versioning for the logs bucket as well
+  versioning {
+    enabled = true
+  }
+}
