7c2161d6256ec828857486cacd2108713bbb722b
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-27 00:29:42 +0530

    changes in new code

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -3,15 +3,17 @@ provider "aws" {
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws",
-  source = "git::ssh://git@github.com:lsdevil4u/TRP-test-repo-module.git"
+  #source = "git::ssh://git@github.com:lsdevil4u/TRP-test-repo-module.git"
 
-  bucket = "my-s3-bucket1112"
+  bucket = "my-s3-bucket12"
   acl    = "private"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
+  
+  force_destroy = false
+  versioning    = true
+  magellan_cors = true
+  
+  
 }
