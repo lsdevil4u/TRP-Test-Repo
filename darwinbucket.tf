2dfa173788979a64ffcdb47f245e8fb06dbe92f8
Ashutosh Kaushik <jecrchai@gmail.com>
2024-05-26 19:02:05 +0530

    creating s3 bucket using terraform

--- darwinbucket.tf
+++ darwinbucket.tf
@@ -3,9 +3,8 @@ provider "aws" {
 region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "digitalrnd-sdi-bioinfo-ireland"
-  acl    = "private"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
 
   tags = {
     Name        = "My bucket"
